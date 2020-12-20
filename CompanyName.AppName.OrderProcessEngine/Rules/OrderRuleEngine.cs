6fa280b47bcf8a15b18aa5cce2047352ec6bead4
Sangeetha <mailtosangee@gmail.com>
2020-12-21 00:08:34 +0530

    TicketId - Inclusion of OrderRuleEngine with all the rulehandlers linked

---
+++ CompanyName.AppName.OrderProcessEngine/Rules/OrderRuleEngine.cs
@@ -0,0 +1,21 @@
+﻿using CompanyName.AppName.OrderProcessEngine.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName.OrderProcessEngine.Rules
+{
+    public class OrderRuleEngine
+    {
+        public void Execute(IOrder order)
+        {
+            var ruleChain = new PhysicalProdOrderRuleHandler();
+            ruleChain.SetNextRule(new BookOrderRuleHandler())
+            .SetNextRule(new MembershipOrderRuleHandler())
+            .SetNextRule(new VideoOrderRuleHandler());
+            ruleChain.Execute(order);
+        }
+    }
+}
