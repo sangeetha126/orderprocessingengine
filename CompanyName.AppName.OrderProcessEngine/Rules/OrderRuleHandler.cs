fcf79674693e3e28b30e7e675d3e22ebb8dbb89b
Sangeetha <mailtosangee@gmail.com>
2020-12-21 00:04:20 +0530

    TicketId - Inclusion of RuleHandlerClass setting the chain of responsiblity

---
+++ CompanyName.AppName.OrderProcessEngine/Rules/OrderRuleHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyName.AppName.OrderProcessEngine.Rules
+{
+    public abstract class OrderRuleHandler<TOrder>
+    {
+        private OrderRuleHandler<TOrder> _nextOrderRuleHandler;
+
+        public OrderRuleHandler<TOrder> SetNextRule(OrderRuleHandler<TOrder> handler)
+        {
+            _nextOrderRuleHandler = handler;
+            return _nextOrderRuleHandler;
+        }
+
+        public virtual void Execute(TOrder order)
+        {
+            if(_nextOrderRuleHandler != null)
+            _nextOrderRuleHandler.Execute(order);
+        }
+
+        public virtual bool IsMatching(TOrder order) { return true; }
+        
+     
+    }
+}
+
